a1a9816f6d391409396f5233f5909dc44fd57dff
maddy1208 <madhanmohanr1208@gmail.com>
2025-10-23 22:54:08 +0530

    updated notes

--- web_dev/node_express/prac.js
+++ web_dev/node_express/prac.js
@@ -1,173 +1,196 @@
-// // // // const path = require('path')
+// // // // // // const path = require('path')
 
-// const { createReadStream } = require('fs')
+// // // const { createReadStream } = require('fs')
 
-// // // const { readFile, writeFile, appendFile, rename } = require('fs')
-// // // const path = require('path')
+// // // // // const { readFile, writeFile, appendFile, rename } = require('fs')
+// // // // // const path = require('path')
 
-// // // // //k
-// // // // console.log("ois")
-// // // // // console.log(global)
-// // // // os=require('os')
-// // // // pat=require('path')
-// // // // console.log(os.type(),os.version(),os.homedir())
-// // // // console.log(__dirname)
-// // // // console.log(__filename)
+// // // // // // //k
+// // // // // // console.log("ois")
+// // // // // // // console.log(global)
+// // // // // // os=require('os')
+// // // // // // pat=require('path')
+// // // // // // console.log(os.type(),os.version(),os.homedir())
+// // // // // // console.log(__dirname)
+// // // // // // console.log(__filename)
 
-// // // // console.log(pat.dirname(__filename),pat.basename(__filename),"\n",pat.extname(__filename))
-// // // // console.log(path.parse(__filename))
+// // // // // // console.log(pat.dirname(__filename),pat.basename(__filename),"\n",pat.extname(__filename))
+// // // // // // console.log(path.parse(__filename))
 
-// // // // math=require('./mod')
-// // // // console.log(math.add(6,2))
+// // // // // // math=require('./mod')
+// // // // // // console.log(math.add(6,2))
 
-// // // file=require('fs')
+// // // // // file=require('fs')
 
-// // // // readFile('./sam.txyyt','utf-8',call);
+// // // // // // readFile('./sam.txyyt','utf-8',call);
 
-// // // // function call(err,data){
-// // // // if (err) throw err;
-// // // // else console.log(data)
+// // // // // // function call(err,data){
+// // // // // // if (err) throw err;
+// // // // // // else console.log(data)
+// // // // // // }
+
+// // // // // // process.on('uncaughtException',(err)=>{
+// // // // // //     console.log("errorru",err);process.exit(1);
+// // // // // // })
+// // // // // filename=path.join(__dirname,'renamed.txt')
+
+// // // // // call1=(err)=>{
+// // // // //     if (err) console.log("erroruuu",err)
+// // // // //     else { console.log("write complete")}
+// // // // //     appendFile(filename,' \nappend=>sample content123s',(err)=>{
+// // // // //         if (err) console.log("append failed")
+// // // // //             else console.log("append success")
+// // // // //     })
+// // // // //     rename(filename,'renamed123.txt',(err=>{
+// // // // //         if (err) console.log("rename failed")
+// // // // //             else console.log("rename success")
+// // // // //     }))
+// // // // // }
+// // // // // writeFile(filename,'sample content123s',call1);
+
+
+
+// // // // fs=require('fs').promises
+
+
+// // // // func= async ()=>{
+// // // //  try {
+// // // //     data=await fs.readFile('./sam.txt',{encoding:'utf-8'})
+// // // //     console.log(data)
+// // // //     await fs.writeFile('./sam.txt','sample file')
+// // // //     console.log("write complete")
+// // // //   await fs.appendFile('./sam.txt','\nappended text')
+// // // //   console.log("append complete")
+// // // //   await fs.rename('./sam.txt','./renamed.txt')
+// // // //   console.log("rename complete")
+
+// // // //     }
+// // // //     catch (err){
+// // // // console.log("error occured", err)
+// // // //     }
 // // // // }
 
-// // // // process.on('uncaughtException',(err)=>{
-// // // //     console.log("errorru",err);process.exit(1);
+// // // // fs=require('fs').promises
+
+// // // // //readfile
+
+
+// // // // //wrirefile
+
+
+// // // // //appednfile
+
+// // // // //renamefile
+
+// // // // func()s
+
+// // // // fs=require('fs')
+
+// // // // fs.createWriteStream('./renamed.txt')
+// // // // .write("jhhgg")
+
+// // // // const event=require('events').EventEmitter
+
+// // // // event1=new event()
+
+// // // // event1.on('greet',()=>{
+// // // //     console.log("greet")
 // // // // })
-// // // filename=path.join(__dirname,'renamed.txt')
 
-// // // call1=(err)=>{
-// // //     if (err) console.log("erroruuu",err)
-// // //     else { console.log("write complete")}
-// // //     appendFile(filename,' \nappend=>sample content123s',(err)=>{
-// // //         if (err) console.log("append failed")
-// // //             else console.log("append success")
-// // //     })
-// // //     rename(filename,'renamed123.txt',(err=>{
-// // //         if (err) console.log("rename failed")
-// // //             else console.log("rename success")
-// // //     }))
+// // // // event1.emit('greet')
+// // // // date=require('date-fns')
+// // // // console.log(date.format(new Date(),"y,M,d,h,m,s"))
+// // // date=require('date-fns')
+// // // const path=require('path')
+// // // const fs=require('fs')
+
+
+// // // logfun=async (msg)=>{
+// // // if(!fs.existsSync(path.join(__dirname,'files','con.txt')))
+// // // {  fs.mkdirSync(path.join(__dirname,'files'))}
+// // //  await fs.promises.appendFile(path.join(__dirname,'files','con.txt'),`${date.format(new Date(),"y,M,d,h,m,s")}\t${msg}    
+// // //  `) 
+// // // console.log("success")
+
 // // // }
-// // // writeFile(filename,'sample content123s',call1);
 
 
-
-// // fs=require('fs').promises
-
-
-// // func= async ()=>{
-// //  try {
-// //     data=await fs.readFile('./sam.txt',{encoding:'utf-8'})
-// //     console.log(data)
-// //     await fs.writeFile('./sam.txt','sample file')
-// //     console.log("write complete")
-// //   await fs.appendFile('./sam.txt','\nappended text')
-// //   console.log("append complete")
-// //   await fs.rename('./sam.txt','./renamed.txt')
-// //   console.log("rename complete")
-
-// //     }
-// //     catch (err){
-// // console.log("error occured", err)
-// //     }
-// // }
-
-// // fs=require('fs').promises
-
-// // //readfile
+// // // module.exports=logfun
 
 
-// // //wrirefile
+// // // http=require('http')
+// // // url=require('url')
 
+// // // http.createServer((req,res)=>{
 
-// // //appednfile
+// // // console.log(url.parse(req.url))
+// // // res.end()
 
-// // //renamefile
+// // // }).listen('9000')
 
-// // func()s
+// // // math=require('./mod')
+// // // console.log(math.mul(8,9))
 
-// // fs=require('fs')
+// // // const http=require('http')
+// // // const fs=require('fs')
+// // // const url=require('url')
 
-// // fs.createWriteStream('./renamed.txt')
-// // .write("jhhgg")
-
-// // const event=require('events').EventEmitter
-
-// // event1=new event()
-
-// // event1.on('greet',()=>{
-// //     console.log("greet")
-// // })
-
-// // event1.emit('greet')
-// // date=require('date-fns')
-// // console.log(date.format(new Date(),"y,M,d,h,m,s"))
-// date=require('date-fns')
-// const path=require('path')
-// const fs=require('fs')
-
-
-// logfun=async (msg)=>{
-// if(!fs.existsSync(path.join(__dirname,'files','con.txt')))
-// {  fs.mkdirSync(path.join(__dirname,'files'))}
-//  await fs.promises.appendFile(path.join(__dirname,'files','con.txt'),`${date.format(new Date(),"y,M,d,h,m,s")}\t${msg}    
-//  `) 
-// console.log("success")
-
-// }
-
-
-// module.exports=logfun
-
-
-// http=require('http')
-// url=require('url')
-
-// http.createServer((req,res)=>{
-
-// console.log(url.parse(req.url))
-// res.end()
-
-// }).listen('9000')
-
-// math=require('./mod')
-// console.log(math.mul(8,9))
-
-// const http=require('http')
-// const fs=require('fs')
-// const url=require('url')
-
-// http.createServer(function(req,res){
-//  path1="."+url.parse(req.url,true).pathname;
-//  console.log("path",path1)
+// // // http.createServer(function(req,res){
+// // //  path1="."+url.parse(req.url,true).pathname;
+// // //  console.log("path",path1)
  
-//  fs.readFile("index.html",(err,data)=>{
+// // //  fs.readFile("index.html",(err,data)=>{
 
-// if(err){
-//     res.writeHead(404,{'content-type':'text/html'});
-//     res.write('./404.html')
-//     res.end()
+// // // if(err){
+// // //     res.writeHead(404,{'content-type':'text/html'});
+// // //     res.write('./404.html')
+// // //     res.end()
 
-// }
-//   res.writeHead(200,{'content-type':'text/html'});
-//   console.log("data",path1)
-//     res.write(data)
-//     res.end()
+// // // }
+// // //   res.writeHead(200,{'content-type':'text/html'});
+// // //   console.log("data",path1)
+// // //     res.write(data)
+// // //     res.end()
 
 
-//  })
+// // //  })
 
     
-// }).listen(9000)
+// // // }).listen(9000)
 
-require("dotenv").config()
-console.log(process.env.pa)
-console.log(`V8 version: ${process.versions.v8}`);
-
-
-const fs=require('fs')
+// // require("dotenv").config()
+// // console.log(process.env.pa)
+// // console.log(`V8 version: ${process.versions.v8}`);
 
 
-console.log(fs.readFileSync('.env','utf-8'))
+// // const fs=require('fs')
 
 
-console.log("after read")
-Promise.resolve().then(() => console.log('3. Promise'));
+// // console.log(fs.readFileSync('.env','utf-8'))
+
+
+// // console.log("after read")
+// // Promise.resolve().then(() => console.log('3. Promise'));
+// const c=require('crypto')
+
+// let hash=c.createHash('sha256').update("sam").digest("hex")
+// let hash1=c.createHash('sha256').update("sam")
+
+// console.log("3",hash,hash1)
+
+
+// const d=c.randomBytes(16).toString('hex')
+// console.log(d)
+
+// console.log()
+console.log(process.platform,process.arch,process.memoryUsage(),process.version)
+console.log(process.env.SAm)
+
+process.on('beforeExit',(code)=>{
+    console.log("code",code)
+})
+
+
+console.log(process.env.API_KEY)
+process.exit(0)
+
